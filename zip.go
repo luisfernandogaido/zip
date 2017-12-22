6827351d25b4f90eec6315e122ce127087715a97
Luís Fernando Gaido <luisfernandogaido@gmail.com>
2017-12-22 12:01:05 -0200

    zip de arquivo. zip de diretório.

--- zip.go
+++ zip.go
@@ -1,34 +1,63 @@
 package zip
 
 import (
-	"os"
-	"archive/zip"
 	"path/filepath"
+	"strings"
 	"io/ioutil"
+	"archive/zip"
+	"os"
+	"fmt"
 )
 
-func File(file string) error {
-	//fixme tudo errado.
-	in, err := os.Open(file)
+//Cria um zip com o arquivo file na pasta destino.
+// O nome do arquivo criado é o mesmo que o de origem, com a extensão trocada para zip.
+func File(arquivoOrigem, pastaDestino string) error {
+	base := filepath.Base(arquivoOrigem)
+	indice := strings.Index(base, filepath.Ext(base))
+	nome := base[:indice]
+	out, err := os.Create(filepath.Join(pastaDestino, nome+".zip"))
 	if err != nil {
 		return err
 	}
-	w := zip.NewWriter(in)
-	f, err := w.Create(filepath.Base(file))
+	w := zip.NewWriter(out)
+	f, err := w.Create(base)
 	if err != nil {
 		return err
 	}
-	bytes, err := ioutil.ReadAll(in)
+	data, err := ioutil.ReadFile(arquivoOrigem)
 	if err != nil {
 		return err
 	}
-	_, err = f.Write(bytes)
+	_, err = f.Write(data)
 	if err != nil {
 		return err
 	}
-	err = w.Close()
+	return w.Close()
+}
+
+//Cria um arquivo com o conteúdo da pasta de origem na pasta de destino.
+// O nome do arquivo é o mesmo que o da pasta de origem.
+func Dir(pastaOrigem, pastaDestino string) error {
+	base := filepath.Base(pastaOrigem)
+	out, err := os.Create(filepath.Join(pastaDestino, base+".zip"))
 	if err != nil {
 		return err
 	}
-	return nil
+	w := zip.NewWriter(out)
+	err = filepath.Walk(pastaOrigem, func(path string, info os.FileInfo, err error) error {
+		if info.IsDir() {
+			return nil
+		}
+		f, err := w.Create(path)
+		if err != nil {
+			return err
+		}
+		data, err := ioutil.ReadFile(path)
+		if err != nil {
+			return err
+		}
+		_, err = f.Write(data)
+		return err
+	})
+	return w.Close()
 }
--- zip_test.go
+++ zip_test.go
@@ -5,7 +5,14 @@ import (
 )
 
 func TestFile(t *testing.T) {
-	err := File("arquivos/arquivo1.txt")
+	err := File("arquivos/arquivo1.txt", "arquivos")
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestDir(t *testing.T) {
+	err := Dir("arquivos", "")
 	if err != nil {
 		t.Fatal(err)
 	}
